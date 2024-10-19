696aa8dff1d37f443cec1f6a4af45e6a03dd2ff2
Matrosov <Matrosov@KOKSFENMETT>
2024-10-19 13:44:14 +0200

    Add project files.

---
+++ №1 (3)/_1__3_.py
@@ -0,0 +1,12 @@
+﻿
+n = int(input("Умова 1<n<9. Введіть кількість рядків n: "))
+
+while(n <= 1 or n >= 9):
+    print("Введене число не задовольняє умові")
+    n = int(input("Введіть ще раз n: "))
+
+for i in range(1, n + 1):
+    for j in range(i, n + 1):
+        print(j, end=" ")
+    print("")
+ 
